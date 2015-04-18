f2929045b8747c5dc3c2bc382a32314fc9655e67
querwurzelt <>
2015-04-18 11:21:31 +0200

    disabled caching for feed settings

--- src/com/wilke/controller/FeedSettings.java
+++ src/com/wilke/controller/FeedSettings.java
@@ -37,6 +37,9 @@ public class FeedSettings extends HttpServlet {
 
 		request.setAttribute("title", aggregate.title);
 		request.setAttribute("feed", feed);
+		response.setHeader("Cache-Control","no-cache"); // HTTP 1.1
+		response.setHeader("Pragma","no-cache"); // HTTP 1.0
+		response.setDateHeader("Expires", 0); // prevents caching at the proxy server
 		request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
 	}
 
